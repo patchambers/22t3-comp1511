38420f78011bc7951f6a9a9b9f962b2e95436ea7
Pat Chambers <z5264081@cse.unsw.edu.au>
2022-09-15 13:19:16 +1000

    Added week 1 code files

---
+++ week01/face.c
@@ -0,0 +1,12 @@
+// Program to print out a smol cat
+// Patrick Chambers, 15/9/22
+
+#include <stdio.h>
+
+int main (void) {
+
+    printf(" /\\___/\\ \n");
+    printf("(=' w '=)");
+    
+    return 0;
+}
